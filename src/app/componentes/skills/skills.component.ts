a772bc781dcc74dfeacd492b210986ec7570d00c
Marcela <mavia2@gmail.com>
2022-08-11 00:19:51 -0300

    modal en skills

--- src/app/componentes/skills/skills.component.ts
+++ src/app/componentes/skills/skills.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PorfolioService } from 'src/app/servicios/porfolio.service';
 import { faPen, faPlusCircle, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import {FormBuilder, FormGroup, Validators } from '@angular/forms';
+declare var window: any;
 
 @Component({
   selector: 'app-skills',
@@ -12,13 +14,80 @@ export class SkillsComponent implements OnInit {
   editIcon = faPen;
   deleteIcon = faTrashAlt;
   addIcon = faPlusCircle
+  formModal: any;
+  formModalEliminar: any;
+  form: FormGroup;
+  esEditar = true;
+  index = 0;
+  tituloEliminar='';
+  mensajeEliminar='';
+  eliminarIndex: number;
 
-  constructor(private datosPorfolio:PorfolioService) { }
+  @Input() estaLogueado: boolean = false;
+
+
+  constructor(private datosPorfolio:PorfolioService, private formBuilder:FormBuilder) {
+    this.form=this.formBuilder.group(
+      {
+        nombre:['',[Validators.required]],
+        porcentaje:['',[Validators.required]],
+
+      }
+    )
+
+
+   }
 
   ngOnInit(): void {
     this.datosPorfolio.obtenerDatos().subscribe(data => {
       this.skillsList=data.skills;
+
     });
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById('modalSkill')
+    );
+    this.formModalEliminar = new window.bootstrap.Modal(
+      document.getElementById('modalEliminarSkill')
+    );
+  }
+
+  openFormModal(type: string, index?: any) {
+    if (type == 'edit') {
+      this.index = index
+      this.esEditar = true
+      this.form.setValue(this.skillsList[index])
+      this.formModal.show();
+    } else {
+      this.esEditar = false
+      this.form.reset()
+      this.formModal.show();
+    }
+  }
+
+  openEliminar( index: any){
+    this.tituloEliminar = 'Eliminar skills';
+    this.mensajeEliminar = `Esta seguro que desea eliminar este skill ${this.skillsList[index].nombre}?`
+    // endpoint delete
+    this.eliminarIndex = index
+    this.formModalEliminar.show();
+  }
+
+  save(event: Event, esEditar: boolean, index?: any) {
+    event.preventDefault
+    if (esEditar) {
+      this.skillsList[index] = this.form.value
+    // guardar cambios en base de datos -> pegarle al endpot de update experiencia
+    } else {
+      this.skillsList.unshift(this.form.value);
+      // agregar experiencia en base de datos -> pegarle al endpot de crear experiencia
+    }
+    this.formModal.hide();
+  }
+
+  eliminar(){
+    delete this.skillsList[this.eliminarIndex];
+    // endpoint delete
+    this.formModalEliminar.hide();
   }
 
 }
